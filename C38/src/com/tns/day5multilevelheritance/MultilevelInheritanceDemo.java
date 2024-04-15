ea8184fff07dcf6f526db0abb62ca0a7266b6329
Divya <Divya@DESKTOP-PLUP371>
2024-04-15 19:40:16 +0530

    Day 5 Task

---
+++ C38/src/com/tns/day5multilevelheritance/MultilevelInheritanceDemo.java
@@ -0,0 +1,16 @@
+package com.tns.day5multilevelheritance;
+
+public class MultilevelInheritanceDemo {
+	public static void main(String[] args) {
+		City city = new City();
+		city.setCityName("Nashik");
+		city.setArea(264.2f);
+		city.setStateName("Maharashtra");
+		city.setLanguage("Marathi");
+		city.setCountryName("India");
+		city.setCapital("Delhi");
+		System.out.println(city);
+		}
+
+
+}
