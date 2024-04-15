8d58ab3d02f2c07e0fc8d2ed26fa4a63e233ee5b
Divya <Divya@172.16.156.163>
2024-04-15 11:26:28 +0530

    Day 2 Task

--- C38/src/com/tns/day2/IfDemo.java
+++ C38/src/com/tns/day2/IfDemo.java
@@ -1,5 +1,10 @@
 package com.tns.day2;
 
 public class IfDemo {
-
+	public static void main(String[] args) {
+		int x = 5;
+		if (x != 5)
+		System.out.println("Value of x is not 5");
+		System.out.println("Value of x is 5");
+		}
 }
